9a00663ca36210111e8d6f98a5fbfffe615cb6d1
Nina Mutty <ninamutty@gmail.com>
2016-10-23 23:08:15 -0700

    completed linked-list problems

--- array-list.rb
+++ array-list.rb
@@ -1,28 +1,50 @@
 # Implementation of a list using a Native array --> ruby does this automatically
+## Can't use .methods (except length)
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0]
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    @storage[@size] = value
+    size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+    #@storage[@size - 1] = 0 ### THIS IS ACTUALLY OPTIONAL! It won't display and it will overwrite.
+    size -= 1
   end
 
   def display
+    toReturn = "["
+    @size.times do |i|
+      toReturn += @storage[i] + ", "
+    end
+    return toReturn + "]"
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
   end
 
   def max
+    return nil if @size == 0  ## ruby already does this?
+    max = @storage[0] ### THIS IS A GOOD PATTERN TO FOLLOW
+    @size.times do |i|
+      max = @storage[i] if @storage[i] > max
+    end
+    return max
   end
 
 end
