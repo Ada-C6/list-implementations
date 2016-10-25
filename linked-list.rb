4d418ab69cfffe00cad5554be882041de0a7fb7c
heatherherrington <redherring@gmail.com>
2016-10-24 19:04:57 -0700

    Wrote include method

--- linked-list.rb
+++ linked-list.rb
@@ -3,91 +3,99 @@
 # LinkedList -> This class holds the linked list functions - adding a node, traversing and displaying the linked list
 
 class Node
-   attr_accessor :value, :next_node
+  attr_accessor :value, :next_node
 
-   def initialize(val,next_in_line=nil)
-       @value = val
-       @next_node = next_in_line
-       puts "Initialized a Node with value:  " + value.to_s
-   end
+  def initialize(val,next_in_line=nil)
+    @value = val
+    @next_node = next_in_line
+    puts "Initialized a Node with value:  " + value.to_s
+  end
 end
 
 class LinkedList
-   def initialize(val)
-       # Initialize a new node at the head
-       @head = Node.new(val)
-   end
+  def initialize(val)
+    # Initialize a new node at the head
+    @head = Node.new(val)
+  end
 
-   def add(value)
-       # Traverse to the end of the list
-       # And insert a new node over there with the specified value
-       # Most common mistake is to move the head, rather than using current to traverse
-       current = @head
-       while current.next_node != nil
-           current = current.next_node # While that node isn't nil, move current to next node
-       end
-       current.next_node = Node.new(value,nil) # Create a new node with current's value
-       self
-   end
+  def add(value)
+    # Traverse to the end of the list
+    # And insert a new node over there with the specified value
+    # Most common mistake is to move the head, rather than using current to traverse
+    current = @head
+    while current.next_node != nil
+      current = current.next_node # While that node isn't nil, move current to next node
+    end
+    current.next_node = Node.new(value,nil) # Create a new node with current's value
+    self
+  end
 
-   def delete(val)
-       current = @head
-       if current.value == val
-           # If the head is the element to be deleted, the head needs to be updated
-           @head = @head.next_node
-       else
-           # ... x -> y -> z
-           # Suppose y is the value to be deleted, you need to reshape the above list to :
-           #   ... x->z
-           # ( and z is basically y.next_node )
-           current = @head
-           while (current != nil) && (current.next_node != nil) && ((current.next_node).value != val)
-               current = current.next_node
-           end
+  def delete(val)
+    current = @head
+    if current.value == val
+      # If the head is the element to be deleted, the head needs to be updated
+      @head = @head.next_node
+    else
+      # ... x -> y -> z
+      # Suppose y is the value to be deleted, you need to reshape the above list to :
+      #   ... x->z
+      # ( and z is basically y.next_node )
+      current = @head
+      while (current != nil) && (current.next_node != nil) && ((current.next_node).value != val)
+        current = current.next_node
+      end
 
-           if (current != nil) && (current.next_node != nil)
-               current.next_node = (current.next_node).next_node
-           end
-       end
-   end
+      if (current != nil) && (current.next_node != nil)
+        current.next_node = (current.next_node).next_node
+      end
+    end
+  end
 
-   def display
-       # Traverse through the list till you hit the "nil" at the end
-       current = @head
-       full_list = []
-       while current.next_node != nil
-           full_list += [current.value.to_s]
-           current = current.next_node
-       end
-       full_list += [current.value.to_s]
-       puts full_list.join("->")
-   end
+  def display
+    # Traverse through the list till you hit the "nil" at the end
+    current = @head
+    full_list = []
+    while current.next_node != nil
+      full_list += [current.value.to_s]
+      current = current.next_node
+    end
+    full_list += [current.value.to_s]
+    puts full_list.join("->")
+  end
 
-   def include?(key)
-   end
+  def include?(key)
+    current = @head
+    reply = false
+    while current.next_node != nil && reply == false
+      reply = true if current.value == key
+      current = current.next_node
+    end
+    reply = true if current.value == key
+    return reply
+  end
 
-   def size
-   end
+  def size
+  end
 
-   def max
-   end
+  def max
+  end
 
 end
 
 # Initializing a Linked List with a node containing value (5)
-ll = LinkedList.new(5)
-
-# Adding Elements to Linked List
-ll.add(10)
-ll.add(20)
-
-# Display the Linked List
-puts "Displaying Linked List:"
-ll.display
-
-puts "Delete 10 and then display the linked list:"
-ll.delete(10)
-ll.display
+# ll = LinkedList.new(5)
+#
+# # Adding Elements to Linked List
+# ll.add(10)
+# ll.add(20)
+#
+# # Display the Linked List
+# puts "Displaying Linked List:"
+# ll.display
+#
+# puts "Delete 10 and then display the linked list:"
+# ll.delete(10)
+# ll.display
 
 =begin
 Output:
